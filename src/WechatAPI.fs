8bd03d63927a28ee9342e0950e3610847c219d18
albert woo <albertwoo_slaveoftime@hotmail.com>
2018-06-25 19:59:24 +0800

    Bug fix for wx api binding

--- src/WechatAPI.fs
+++ src/WechatAPI.fs
@@ -1,13 +1,7 @@
 ﻿module WechatAPI
 
 open Fable.Core
-open Fable.Import.Browser
-open Fable.Core.JsInterop
 
-type [<AllowNullLiteral>] WX =
-    [<Emit("$0($1...)")>] abstract createCanvas: unit -> HTMLCanvasElement
-    [<Emit("$0($1...)")>] abstract setPreferredFramesPerSecond: int -> unit
-let [<Global>] wx: WX = createEmpty
-
-type [<AllowNullLiteral>] Audio = inherit HTMLAudioElement
-type [<AllowNullLiteral>] Image = inherit HTMLImageElement
+type [<AllowNullLiteral>] IWxApi =
+    abstract setPreferredFramesPerSecond: int -> unit
+let [<Global>] wx: IWxApi = jsNative
